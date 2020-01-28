55af6264e748ea9784799d06989a4c6b811f6fb1
Jenise Ruehle <jela07@gmail.com>
2020-01-28 02:04:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,9 +15,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\A[A-Z]\.\W\b/)
-    
-  
+  if text.match(/\A[A-Z]\w*\W\b/)
+    return true 
+  else
+    false
+  end   
 end
 
 def valid_phone_number?(phone)
